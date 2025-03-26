4c3533c4fa04247b926878eb81f9fa4f311fc97f
TakeruDavis <1822248+TakeruDavis@users.noreply.github.com>
2025-03-26 21:05:43 +0100

    Initial commit

---
+++ src/main/java/net/TakeruDavis/yourmodid/register/ModPonder.java
@@ -0,0 +1,14 @@
+package net.TakeruDavis.yourmodid.register;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import net.minecraftforge.data.event.GatherDataEvent;
+
+public class ModPonder {
+    public static void register() {
+        // Put your ponder here!
+    }
+
+    public static void generateLang(CreateRegistrate registrate, GatherDataEvent event) {
+        register();
+    }
+}
